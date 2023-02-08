6e827dd4540fe4c6e7d68ca138b0c3c58a899c0b
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-08 16:05:47 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,70 @@
+export const item = {
+    "id": "71334400-c9fc-4790-84a7-2ab6b8715b1c",
+    "recommendation": "Work itself increase wide interesting. Item relationship spend box among majority. Behind trade life approach TV.",
+    "art_fairs": [
+        {
+            "id": "95f9ee3f-dc17-4302-a00a-aa131bff23a7",
+            "art_fair": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/49.png"
+        },
+        {
+            "id": "1616e329-347f-45ee-8907-ba2c9dcf56b9",
+            "art_fair": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/276.png"
+        },
+        {
+            "id": "c7f8acf3-35d3-47d4-8dbc-a4825ed74e0c",
+            "art_fair": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/186.png"
+        },
+        {
+            "id": "38f3f75a-c989-43a4-9eeb-ef8e47babd92",
+            "art_fair": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/332.png"
+        },
+        {
+            "id": "cae2a90f-512e-4fd5-ae50-5e9d209bc99c",
+            "art_fair": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/306.png"
+        }
+    ],
+    "keith_haring": "Step make see local seek floor image. Bar effort could history report speak strategy. Already suggest decade down as record. Trade law environment international feel former.",
+    "keith_haring_list": [
+        {
+            "id": "1abaff5a-8e7b-48cd-bb0f-41fc506d93d1",
+            "pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/153.png",
+            "price": 7668,
+            "name": "we",
+            "story": "High improve letter room particularly. Concern none nor move want.",
+            "about_keith_haring": "Factor will minute natural music specific culture. Lead security animal area region direction. Well unit chair in."
+        },
+        {
+            "id": "14a08029-60c8-480c-9c2d-780698db57da",
+            "pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/137.png",
+            "price": 6639,
+            "name": "song",
+            "story": "Third us attorney follow trade research soldier. Describe nothing make of.",
+            "about_keith_haring": "Occur name special win admit political. Plant practice side order well recent."
+        },
+        {
+            "id": "76dcb542-556d-4fdd-ae0c-be0c613a907b",
+            "pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/186.png",
+            "price": 5130,
+            "name": "kind",
+            "story": "Major pull plan. Would new turn road discuss majority least. Level usually similar record.",
+            "about_keith_haring": "Like forget fear artist skill. Range wife paper option simply commercial second."
+        },
+        {
+            "id": "b97c51af-abc4-469f-b152-905a910189d7",
+            "pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/279.png",
+            "price": 106,
+            "name": "task",
+            "story": "Nothing speech loss agent. Ability traditional past conference. Yard gas some important.",
+            "about_keith_haring": "Direction north edge country dark Congress she major. Audience general along someone behavior."
+        },
+        {
+            "id": "3b5cd827-e97a-4e6c-8ba5-f08fe368a7bc",
+            "pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/105.png",
+            "price": 3231,
+            "name": "option",
+            "story": "Environmental part key know. College local home fund own strong. Region model high.",
+            "about_keith_haring": "Claim deal television floor central strategy necessary. Act near memory fast nearly young. Brother write production likely blood partner."
+        }
+    ],
+    "catchy_line": "Describe nice daughter season issue call word official. Peace house whatever growth mouth. Probably almost side sort begin price discuss. Your country wear scene."
+}
